8063f396b9645f76b02aaeacbacdb468d6e42a56
eric birkeland <ericjbirkeland@gmail.com>
2018-05-03 13:46:07 -0400

    window.alert word count

--- src/js/main.ts
+++ src/js/main.ts
@@ -5,16 +5,17 @@ const msgEl = document.getElementById("message");
 // form is invalid if no text was entered in inputEl
 // truth value is set when count button is clicked
 var formIsValid = true;
+var wordCount = 0;
 
-buttonEl.addEventListener("click", handleClick, false);
+buttonEl.addEventListener("click", handleButtonClick, false);
 
-function handleClick (event) {
-    let inputText = inputEl.value;
-    // regex to check for at least one non-white-space char
-    if (formIsValid !== /\S/gm.test(inputText)) {
+function handleButtonClick (event: MouseEvent) {
+    // regex to check for at least one word char
+    if (formIsValid !== /\w/gm.test(inputEl.value)) {
         formIsValid = !formIsValid;
         requestAnimationFrame(updateErrorStatus);    
     }
+    if (formIsValid) setTimeout(countWords, 0);
 }
 
 function updateErrorStatus () {
@@ -25,4 +26,13 @@ function updateErrorStatus () {
         inputEl.className = "form-error";
         msgEl.innerText = "text input required";
     }
+}
+
+function countWords (text: string) {
+    wordCount = inputEl.value.match(/\w+/g).length;
+    requestAnimationFrame(showWordCount);
+}
+
+function showWordCount () {
+    window.alert(`Word count: ${wordCount}`);
 }
